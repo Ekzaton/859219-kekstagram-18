02ae3a9d8410a808a7ed17e6511f0b36954f1986
Ekzaton <ekzaton@gmail.com>
2019-10-01 23:39:37 +0500

    Правки для списка 2

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 
 // Константы
 var COMMENT = {
-  message: [
+  messages: [
     'Всё отлично!',
     'В целом всё неплохо. Но не всё.',
     'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
     'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
     'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
     'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'],
-  name: [
+  names: [
     'Имечко Фамилия',
     'Пётр Николаевич',
     'Васисуалий Лоханкин',
@@ -23,47 +23,35 @@ var NUMBER_OF_PICTURES = 25;
 var picturesListElement = document.querySelector('.pictures');
 var picturesItemElement = document.querySelector('#picture').content.querySelector('.picture');
 
-// Генерация случайного числа из интервала (minNumber - начало интервала, maxNumber - конец интервала)
+// Генерация случайного числа из интервала
 var getRandomNumber = function (minNumber, maxNumber) {
   var randomNumber = Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
 
   return randomNumber;
 };
 
-// Генерация индекса случайного элемента массива (array - массив)
+// Генерация индекса случайного элемента массива
 var getRandomIndex = function (array) {
   var randomIndex = array[Math.floor((Math.random() * array.length - 1))];
 
   return randomIndex;
 };
 
-// Генерация индекса уникального случайного элемента массива (array - массив)
-var getRandomUniqueIndex = function (array) {
-  var randomUniqueIndex = array[Math.floor((Math.random() * array.length - 1))];
+// Генерация аватаров пользователей
+var getUserAvatars = function () {
+  var userAvatars = 'img/avatar-' + getRandomNumber(1, 6) + '.svg';
 
-  return array.splice(randomUniqueIndex, 1);
+  return userAvatars;
 };
 
-// Генерация URL фотографий (number - количество фотографий)
-var getUrls = function (number) {
-  var urls = [];
-
-  for (var i = 0; i < number; i++) {
-    var newUrl = 'photos/' + (i + 1) + '.jpg';
-    urls.push(newUrl);
-  }
-
-  return urls;
-};
-
-// Генерация комментариев (number - количество комментариев)
-var getComments = function (number) {
+// Генерация комментариев пользователей
+var getUserComments = function (numberOfComments) {
   var comments = [];
 
-  for (var i = 0; i < number; i++) {
-    var avatar = getAvatars();
-    var message = getRandomIndex(COMMENT.message);
-    var name = getRandomIndex(COMMENT.name);
+  for (var i = 0; i < numberOfComments; i++) {
+    var avatar = getUserAvatars();
+    var message = getRandomIndex(COMMENT.messages);
+    var name = getRandomIndex(COMMENT.names);
 
     var newComment = {
       avatar: avatar,
@@ -77,22 +65,14 @@ var getComments = function (number) {
   return comments;
 };
 
-// Генерация аватаров ()
-var getAvatars = function () {
-  var avatars = 'img/avatar-' + getRandomNumber(1, 6) + '.svg';
-
-  return avatars;
-};
-
-// Генерация данных для фотографий (number - количество фотографий)
-var getPictureData = function (number) {
+// Генерация данных для фотографий
+var getPictureData = function (numberOfPictures) {
   var pictureData = [];
-  var urls = getUrls(number).slice();
 
-  for (var i = 0; i < number; i++) {
-    var url = getRandomUniqueIndex(urls);
+  for (var i = 0; i < numberOfPictures; i++) {
+    var url = 'photos/' + (i + 1) + '.jpg';
     var likes = getRandomNumber(15, 200);
-    var comments = getComments(getRandomNumber(1, 5));
+    var comments = getUserComments(getRandomNumber(1, 5));
 
     var newPicture = {
       url: url,
@@ -106,7 +86,7 @@ var getPictureData = function (number) {
   return pictureData;
 };
 
-// Создание фотографии (picture - фотография)
+// Создание фотографии
 var createPicture = function (picture) {
   var picturesItem = picturesItemElement.cloneNode(true);
 
@@ -117,7 +97,7 @@ var createPicture = function (picture) {
   return picturesItem;
 };
 
-// Создание списка фотографий (pictureData - данные фотографии)
+// Создание списка фотографий
 var createPicturesList = function (pictureData) {
   var picturesList = document.createDocumentFragment();
 
@@ -125,12 +105,12 @@ var createPicturesList = function (pictureData) {
     picturesList.appendChild(createPicture(pictureData[i]));
   }
 
-  return picturesListElement.appendChild(picturesList);
+  return picturesList;
 };
 
-// Отрисовка списка фотографий (items - фотографии)
-var renderPictures = function (items) {
-  createPicturesList(getPictureData(items));
+// Отрисовка списка фотографий
+var renderPictures = function (numberOfPictures) {
+  picturesListElement.appendChild(createPicturesList(getPictureData(numberOfPictures)));
 };
 
 renderPictures(NUMBER_OF_PICTURES);
