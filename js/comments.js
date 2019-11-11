7565b616bae9ea4df31761824e8ba658dcf071f1
Ekzaton <ekzaton@gmail.com>
2019-11-11 21:54:27 +0500

    Fix3

--- js/comments.js
+++ js/comments.js
@@ -25,43 +25,37 @@
 
   // Создание списка комментариев
   var createSocialCommentsList = function (comments) {
-    socialCommentsLoaderElement.classList.remove('visually-hidden');
+    socialCommentsListElement.innerHTML = '';
+    socialCommentsListElement.appendChild(splitUpComments(comments));
+  };
 
+  // Разделение комментариев
+  var splitUpComments = function (comments) {
     var socialCommentsList = document.createDocumentFragment();
     if (comments.length <= COMMENTS_LIMIT) {
       comments.forEach(function (comment) {
         socialCommentsList.appendChild(getSocialCommentsItem(comment));
       });
       socialCommentsLoaderElement.classList.add('visually-hidden');
+      socialCommentsLoaderElement.removeEventListener('click', onSocialCommentsLoaderClick);
+
     } else {
-      for (var i = 0; i < COMMENTS_LIMIT; i++) {
-        socialCommentsList.appendChild(getSocialCommentsItem(comments[i]));
-      }
+      comments.forEach(function (comment, index) {
+        if (index < COMMENTS_LIMIT) {
+          socialCommentsList.appendChild(getSocialCommentsItem(comment));
+        }
+      });
       hiddenComments = comments.slice(COMMENTS_LIMIT);
+      socialCommentsLoaderElement.classList.remove('visually-hidden');
       socialCommentsLoaderElement.addEventListener('click', onSocialCommentsLoaderClick);
     }
 
-    socialCommentsListElement.innerHTML = '';
-    socialCommentsListElement.appendChild(socialCommentsList);
+    return socialCommentsList;
   };
 
   // Загрузка скрытых комментариев
   var onSocialCommentsLoaderClick = function () {
-    var socialCommentsList = document.createDocumentFragment();
-    if (hiddenComments.length <= COMMENTS_LIMIT) {
-      hiddenComments.forEach(function (comment) {
-        socialCommentsList.appendChild(getSocialCommentsItem(comment));
-      });
-      socialCommentsLoaderElement.classList.add('visually-hidden');
-      socialCommentsLoaderElement.removeEventListener('click', onSocialCommentsLoaderClick);
-    } else {
-      for (var i = 0; i < COMMENTS_LIMIT; i++) {
-        socialCommentsList.appendChild(getSocialCommentsItem(hiddenComments[i]));
-      }
-      hiddenComments = hiddenComments.slice(COMMENTS_LIMIT);
-    }
-
-    socialCommentsListElement.appendChild(socialCommentsList);
+    socialCommentsListElement.appendChild(splitUpComments(hiddenComments));
   };
 
   // Экспорт
